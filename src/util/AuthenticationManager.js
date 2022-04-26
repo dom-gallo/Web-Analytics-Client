26d63f928cab6af81509e6e369f1f015f6aad5be
Dominic Gallo <dgallo519@protonmail.com>
2022-04-25 21:19:02 -0400

    fixed cors issue, trying to get JWT token from response header.

--- src/util/AuthenticationManager.js
+++ src/util/AuthenticationManager.js
@@ -6,16 +6,34 @@ class AuthenticationManager{
     constructor() {
         this.loginUrl = "http://localhost:8080/login";
     }
+
     // returns a JWT
     doLogin = (emailAddress, password) => {
-        fetch(this.loginUrl)
+        console.log(`Email: ${emailAddress}\n Password: ${password}`)
+        const data = {
+            emailAddress : emailAddress,
+            password : password
+        },
+
+        options = {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            redirect: "follow",
+            referrerPolicy : "no-referrer",
+            body: JSON.stringify({emailAddress : emailAddress, password: password})
+        };
+        fetch(this.loginUrl,options)
             .then(response => {
                 console.log("Response from server below ---")
-                console.log(response);
-                response.json();
+                console.log("response headers");
+                console.log(response.headers.get("Connection"));
             }).then((result) => {
-                //  This should be the JWT
-                return result;
+                console.log("Result headers")
+                console.log(result)
         },
             (error) => {
                 // just alert the error;
