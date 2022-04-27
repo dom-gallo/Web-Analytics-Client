34910c17b3b49f1d67cc5ff3db1e7d7c74f316d9
Dominic Gallo <dgallo519@protonmail.com>
2022-04-26 21:07:46 -0400

    button functionality

--- src/components/Logout_GetStarted.js
+++ src/components/Logout_GetStarted.js
@@ -1,12 +1,15 @@
-const Logout_GetStarted = (props) => {
+import {useNavigate} from "react-router-dom";
 
+
+const Logout_GetStarted = (props) => {
+    const navigate = useNavigate();
     if(localStorage.getItem("authToken")){
         return (
             <>
                     <button type="button"
                             onClick={(event) => {
                                 event.preventDefault();
-                                console.log("Logout pressed");
+                                navigate("/logout");
                             }}
                             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Logout
@@ -19,7 +22,7 @@ const Logout_GetStarted = (props) => {
                 <button type="button"
                         onClick={(event) => {
                             event.preventDefault();
-                            console.log("Get Started pressed");
+                            navigate("/signup");
                         }}
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Get Started
